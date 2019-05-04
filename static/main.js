cbd9d02e4338dbf4cbf92ac37777ef5b0311e04c
Kirill Bakhtin <flash232323@gmail.com>
2019-05-04 21:13:14 +0600

    all methods added

--- static/main.js
+++ static/main.js
@@ -1,11 +1,13 @@
+"use strict"
+
 class Profile {
-    constructor(username, {firstname, lastname}, password) {
+    constructor({ username, name: { firstName, lastName }, password}) {
         this.username = username;
         this.name = {firstname, lastname};
         this.password = password;
     };
 
-    create(callback) {
+    createUser (callback) {
         return ApiConnector.createUser (
             {
             username: this.username,
@@ -19,11 +21,70 @@ class Profile {
         ) 
     };
 
-    logIn({username, password}, callback) {
+    performLogin (callback) {
+        return ApiConnector.performLogin (
+            {
+                username: this.username,
+                password: this.password,
+            },
+            (err, data) => {
+                console.log(`Authorizing user ${this.username}`);
+                callback (err, data);
+            }
+        )
 
+    };
+
+    addMoney ({currency, amount}, callback) {
+        return ApiConnector.addMoney (
+            {currency, amount},
+            (err, data) => {
+                console.log(`Adding ${amount} of ${curency} to ${this.username}`);
+                callback (err, data);
+            }
+        )
+    };
+
+    convertMoney ({fromCurrency, toCurrency, targetAmount}, callback) {
+        return ApiConnector.convertMoney (
+            {fromCurrency, toCurrency, targetAmount},
+            (err, data) => {
+                console.log(`Converting ${fromCurrency} to ${targetAmount} ${toCurrency}`);
+                callback (err, data);
+            }
+        )
+    };
+
+    transferMoney ({to, amount}, callback) {
+        return ApiConnector.transferMoney (
+            {to, amount},
+            (err, data) => {
+                console.log(`Transfering ${amount} to ${to}`);
+                callback (err, data);
+            }
+        )
     }
 };
 
-const ivan = new Profile('Ivan', {firstname: 'Ivan', lastname: 'Fedorov'}, 123456);
+function getStocks (callback) {
+    return ApiConnector.getStocks (
+        (err, data) => {
+            console.log (`Getting stock info`);
+            callback (err, data)
+        }
+    )
+};
 
-console.log(ivan);
+
+    
+    const money = { currency: 'EUR', amount: 500000 };
+
+    getStocks((err, data) => {
+        if (err) {
+            console.error('Error during getting stocks');
+        }
+        const stocksInfo = data[0];
+        console.log (stocksInfo);
+    });
+
+    
